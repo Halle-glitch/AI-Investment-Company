4c3274ae270882056260ccb1b759be58e92347d9
Halle-glitch <rock.areias@gmail.com>
2026-08-08 16:33:58 +0100

    feat: initialize AI Investment Company

---
+++ main.py
@@ -0,0 +1,9 @@
+print("AI Investment Company")
+
+class FundamentalAnalyst:
+    def analyse(self, company):
+        print("Analysing company:", company)
+
+analyst = FundamentalAnalyst()
+
+analyst.analyse("Microsoft")
