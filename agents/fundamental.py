ef05dbe29646ecdd09ebf203c2839209be2005c4
Halle-glitch <rock.areias@gmail.com>
2026-08-08 21:46:12 +0100

    feat: add neutral, testing

--- agents/fundamental.py
+++ agents/fundamental.py
@@ -1,11 +1,20 @@
 class FundamentalAnalyst:
     def analyse(self, company, revenue_growth, profit_margin, debt):
+        conclusion = "0"
+        if revenue_growth > 0 and profit_margin > 10 and debt < 50:
+            conclusion = "Bullish"
+        elif revenue_growth < 0 and profit_margin < 10 and debt > 50:
+            conclusion = "Bearish"
+        else:
+            conclusion = "Neutral"
+
+        
         return {
             "company": company,
             "revenue_growth": revenue_growth,
             "profit_margin": profit_margin,
             "debt": debt,
-            "conclusion": "Bullish" if revenue_growth > 0 and profit_margin > 10 and debt < 50 else "Bearish"
+            "conclusion": conclusion  
         }
 
 
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ print("AI Investment Company")
 
 analyst = FundamentalAnalyst()
 
-result = analyst.analyse("Microsoft", 12, 15, 51)
+result = analyst.analyse("Microsoft", 12, 15, 30)
+result1 = analyst.analyse("Microsoft", -5, 5, 70)
+result2 = analyst.analyse("Microsoft", 12, 5, 30)
 print(result)
-
+print(result1)
+print(result2)
