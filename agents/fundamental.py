0bd3a56b79b207e9cf941596693802236336d22d
Halle-glitch <rock.areias@gmail.com>
2026-08-09 17:37:30 +0100

    feat: add risk based conclusion

--- agents/fundamental.py
+++ agents/fundamental.py
@@ -1,13 +1,25 @@
 class FundamentalAnalyst:
     def analyse(self, company, revenue_growth, profit_margin, debt):
 
+
+        if debt < 30:
+            risk_level = "Low risk"
+        elif debt >= 30 and debt <= 50:
+            risk_level = "Medium risk"
+        else:
+            risk_level = "High risk"
+
+
         conclusion = ""
         if revenue_growth > 0 and profit_margin > 10 and debt < 50:
             conclusion = "Bullish"
         elif revenue_growth < 0 and profit_margin < 10 and debt > 50:
             conclusion = "Bearish"
+        elif revenue_growth > 0 and profit_margin > 10 and risk_level == "High risk":
+            conclusion = "Neutral"
         else:
             conclusion = "Neutral"
+
 
         reason = ""
         if revenue_growth > 0 and profit_margin > 10 and debt < 50:
@@ -23,8 +35,10 @@ class FundamentalAnalyst:
             "revenue_growth": revenue_growth,
             "profit_margin": profit_margin,
             "debt": debt,
-            "conclusion": conclusion,
-            "reason": reason  
+            "risk_level": risk_level,
+            "reason": reason,
+            "conclusion": conclusion
+         
         }
 
 
--- main.py
+++ main.py
@@ -4,9 +4,11 @@ print("AI Investment Company")
 
 analyst = FundamentalAnalyst()
 
-result = analyst.analyse("Microsoft", 12, 15, 30)
+result = analyst.analyse("Microsoft", 12, 15, 20)
 result1 = analyst.analyse("Microsoft", -5, 5, 70)
 result2 = analyst.analyse("Microsoft", 12, 5, 30)
+result3 = analyst.analyse("Microsoft", 12, 15, 70)
 print(result)
 print(result1)
 print(result2)
+print(result3)
